f5ecf2f05500b37c8d2fdf628e26dcd9b60913a3
MNaufal2506 <naufaljamil48@gmail.com>
2025-09-19 11:21:43 +0700

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: "class",
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        "f-color": "#A0E1FF",
+        "sen-red": "#680000",
+        "red-gelap":"#5A0202"
+      },
+      animation: {
+        bounce: "bounce 2s infinite",
+      },
+      fontFamily:{
+        "josep": 'Josefin Sans'
+      }
+    },
+  },
+  plugins: [],
+};
